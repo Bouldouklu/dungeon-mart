67875418197a2e69f80be77171bce08aa0c5c344
Bouldouklu <75644130+Bouldouklu@users.noreply.github.com>
2025-10-06 11:21:55 +0200

    Add money display UI

---
+++ Assets/Scripts/MoneyDisplay.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class MoneyDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI moneyText;
+
+    private void Start()
+    {
+        UpdateDisplay();
+    }
+
+    private void Update()
+    {
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        if (GameManager.Instance != null && moneyText != null)
+        {
+            moneyText.text = "$" + GameManager.Instance.CurrentMoney;
+        }
+    }
+}
